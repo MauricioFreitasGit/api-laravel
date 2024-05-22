8dc4968a577a664fc2521de14ed89ba6ecc1f131
Mauricio Freitas <mauricio@MBP-de-Mauricio.bbrouter>
2024-05-22 08:20:56 -0300

    teste

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,12 +30,9 @@ return [
     |
     */
 
-    'connections' => [
-
-        'pusher' => [
+    'connections' => ['pusher' => [
             'driver'  => 'pusher',
-            'key'     => env('PUSHER_APP_KEY'),
-            'secret'  => env('PUSHER_APP_SECRET'),
+            'key'     => env('PUSHER_APP_KEY'),'secret'  => env('PUSHER_APP_SECRET'),
             'app_id'  => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster'   => env('PUSHER_APP_CLUSTER'),
