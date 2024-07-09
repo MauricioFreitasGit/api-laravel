b1bde0ad42e286df2f3a50dc07198a90c264e1d3
Mauricio Freitas <mauricio@MBP-de-Mauricio.bbrouter>
2024-07-09 19:57:09 -0300

    ola

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,11 +7,12 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\{RateLimiter, Route};
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    public const HOME = '/home';
+    //public const HOME = '/home';
 
     public function boot(): void
     {
