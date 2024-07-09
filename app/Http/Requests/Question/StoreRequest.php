f065a4e8e7ec2219200bd5410f55cd455bd7cded
Mauricio Freitas <mauricio@MBP-de-Mauricio.bbrouter>
2024-07-09 20:15:15 -0300

    validation rules question required

--- app/Http/Requests/Question/StoreRequest.php
+++ app/Http/Requests/Question/StoreRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Http\Requests\Question;
 
@@ -24,7 +24,7 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'question' => 'required'
         ];
     }
 }
--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Tests\Feature\Question;
 
@@ -40,4 +40,17 @@ test('after creating a new question, I need to make sure that it creates on _dra
         'status'   => 'draft',
         'question' => 'Lorem ipusn Jeremias?',
     ]);
+});
+
+describe('validation rules', function () {
+    test('question required', function () {
+        $user = User::factory()->create();
+
+        //utilizando para logar
+        Sanctum::actingAs($user);
+    
+        postJson(route('questions.store',[]))->assertJsonValidationErrors([
+            'question'=>'required'
+        ]);
+    });
 });
