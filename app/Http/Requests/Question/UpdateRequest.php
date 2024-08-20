684513f08fa212cff146c49484e572b4867a7e4c
Mauricio Freitas <mauricio@MBP-de-Mauricio.bbrouter>
2024-08-20 11:36:09 -0300

    authorization makink sure that only user that has created

--- app/Http/Requests/Question/UpdateRequest.php
+++ app/Http/Requests/Question/UpdateRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Question;
 
 use App\Rules\WithQuestionMark;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 /**
@@ -16,7 +17,9 @@ class UpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        /** @var Question $question */
+        $question = $this->route()->question; // @phpstan-ignore-line
+        return Gate::allows('update', $question);
     }
 
     /**
